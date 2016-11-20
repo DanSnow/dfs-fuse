8afe7b15be5bcd333dbb71ec7c3621a5084863fd
DanSnow <dododavid006@gmail.com>
2016-11-20 16:06:08 +0800

    refactor(memoryfs): Update

--- dfsfuse/dfsfuse/memoryfs.py
+++ dfsfuse/dfsfuse/memoryfs.py
@@ -6,9 +6,9 @@ class MemoryFS:
 
   def reset(self):
     self._meta = {}
-    self._dirs = {}
+    self._paths = {}
 
-  def hasdir(self, path):
+  def has(self, path):
     if path in self._meta:
       return True
     return False
@@ -16,18 +16,27 @@ class MemoryFS:
   def readdir(self, path):
     if path not in self._meta:
       raise TypeError('Path not exist')
-    return self._meta[path]
+    return self._paths[path]
+
+  def isdir(self, path):
+    return self.has(path) and self._meta[path]['type'] == 'dir'
+
+  def isfile(self, path):
+    return self.has(path) and self._meta[path]['type'] == 'file'
 
   def adddir(self, path, content):
-    self._meta[path] = content
-    if path not in self._dirs:
-      self._dirs[path] = content['.']['id']
-    for name, meta in content.items():
-      if name == '.' or name == '..':
-        continue
+    self._paths[path] = content['.']
+    self._meta[path] = content['.']
+    self._meta[path].update({
+      'children': set()
+    })
 
-      if meta['type'] == 'dir':
-        self._dirs[os.path.join(path, name)] = meta['id']
+    for name, meta in content.items():
+      if name == '..' or name == '.':
+        continue
+      child_path = os.path.join(path, name)
+      self._paths[child_path] = meta
+      self._meta[path]['children'].add(child_path)
 
   def getid(self, path):
-    return self._dirs[path]
+    return self._paths[path]['id']
