ccbb2ab3cfce70419f8342abdc5c9a0be4a908f4
DanSnow <dododavid006@gmail.com>
2016-11-21 00:19:10 +0800

    feat: Parse options

--- dfsfuse/main.py
+++ dfsfuse/main.py
@@ -1,16 +1,43 @@
+import argparse
 from logging import getLogger
 from fuse import FUSE
 from dfsfuse import DFSFuse, Client
-from sys import argv
 
-def run_fuse():
+def run_fuse(args):
   logger = getLogger('FUSE')
   logger.info('Run fuse')
-  client = Client(host='140.123.241.61')
+  client = Client(host = args.host, port = args.port, psk = args.key)
 
-  fuse = FUSE(DFSFuse(client), argv[1], foreground=True)
+  fuse = FUSE(DFSFuse(client), args.mount, foreground=True)
 
 def main(argv):
   logger = getLogger('main')
-  run_fuse()
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    'mount',
+    help = 'Mount point',
+    type = str
+  )
+  parser.add_argument(
+    '-k',
+    '--key',
+    help = 'PSK key',
+    default = '',
+    type = str
+  )
+  parser.add_argument(
+    '--host',
+    help = 'Host',
+    default = 'localhost',
+    type = str
+  )
+  parser.add_argument(
+    '-p',
+    '--port',
+    help = 'Port',
+    default = 4096,
+    type = int
+  )
+  args = parser.parse_args()
+  run_fuse(args)
 
