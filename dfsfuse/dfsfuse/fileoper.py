c122c0dda9c22054610ad47d761ec0e075fe02ea
DanSnow <dododavid006@gmail.com>
2016-11-23 21:10:20 +0800

    fix: Fix #write

--- dfsfuse/dfsfuse/fileoper.py
+++ dfsfuse/dfsfuse/fileoper.py
@@ -4,9 +4,10 @@ def read(content, offset, length):
   return content[offset : offset + length]
 
 def write(content, data, offset):
+  # FIXME: Maybe should use bytes here
   buf = StringIO(content)
   buf.seek(offset)
-  buf.write(data)
+  buf.write(data.decode('utf-8'))
   return buf.getvalue()
 
 def truncate(content, length):
