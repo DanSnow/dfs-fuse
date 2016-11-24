f6f6c94fb4efe45a70bf7dc4e6cd2955355c7228
DanSnow <dododavid006@gmail.com>
2016-11-24 22:59:41 +0800

    feat: Split socket

--- dfsfuse/dfsfuse/client.py
+++ dfsfuse/dfsfuse/client.py
@@ -5,6 +5,7 @@ import collections
 import hashlib
 import socket
 import json
+from functools import wraps
 from hirlite import Rlite
 from .packet import Packet
 from .memoryfs import MemoryFS
@@ -13,6 +14,16 @@ from .exception import TimeoutError, ServerError, InternalError, DisconnectError
 
 logger = getLogger('Client')
 socket.setdefaulttimeout(5)
+
+def inject_socket(func):
+  @wraps(func)
+  def _wrapper(*args, **kargs):
+    self = args[0]
+    sock = self._connect()
+    res = func(self, sock, *args[1:])
+    sock.close()
+    return res
+  return _wrapper
 
 class Client():
   def __init__(self, host = 'localhost', port = 4096, psk = '', cache = True):
@@ -27,17 +38,19 @@ class Client():
     self._init()
 
 
-  def login(self):
+  @inject_socket
+  def login(self, sock):
     logger.info('Send login')
-    _, body = self.request('auth#login', header = { 'psk': self._psk  })
+    _, body = self.request(sock, 'auth#login', header = { 'psk': self._psk  })
     if body != b'OK':
       logger.error('Login fail')
       raise AuthError('Login fail')
     logger.info('Login success')
 
-  def ping(self):
+  @inject_socket
+  def ping(self, sock):
     logger.info('Ping')
-    _, body = self.request('echo#echo', body = b'ping')
+    _, body = self.request(sock, 'echo#echo', body = b'ping')
     if body != 'ping':
       logger.error('Ping fail')
       raise ServerError('Ping: unexpected response')
@@ -49,7 +62,8 @@ class Client():
       return self._fs.getmeta(path)
     raise TypeError('Path not exist')
 
-  def has(self, path):
+  @inject_socket
+  def has(self, sock, path):
     deque = collections.deque()
     cur_path = path
 
@@ -74,7 +88,8 @@ class Client():
           break
     return self._fs.has(path)
 
-  def write(self, path, content):
+  @inject_socket
+  def write(self, sock, path, content):
     parent_path = os.path.dirname(path)
     if not self._fs.isdir(parent_path):
       raise RuntimeError('Write: path is not dir')
@@ -82,35 +97,38 @@ class Client():
     name = os.path.basename(path)
     id = self._fs.getid(parent_path)
     logger.info('Write to %s, content len %s', path, len(content))
-    _, body = self.request('file#put', header = { 'id': id, 'name': name }, body = content)
+    _, body = self.request(sock, 'file#put', header = { 'id': id, 'name': name }, body = content)
     if body != b'OK':
       raise ServerError('Write fail')
-    self._readdir(parent_path)
+    self._readdir(sock, parent_path)
     self._fs.loadfile(path, content)
     return True
 
-  def read(self, path):
+  @inject_socket
+  def read(self, sock, path):
     if not self._fs.isfile(path):
       return None
     id = self._fs.getid(path)
-    header, body = self.request('file#get', header = { 'id': id })
+    header, body = self.request(sock, 'file#get', header = { 'id': id })
     if header['result'] != 'OK':
       raise ServerError('Read fail')
     self._fs.loadfile(path, body)
     return body
 
-  def rm(self, path):
+  @inject_socket
+  def rm(self, sock, path):
     if not self._fs.isfile(path):
       return False
     parent_path = os.path.dirname(path)
     id = self._fs.getid(path)
-    header, body = self.request('file#rm', header = { 'id': id })
+    header, body = self.request(sock, 'file#rm', header = { 'id': id })
     if body != b'OK':
       raise ServerError('Rm fail')
-    self._readdir(parent_path)
+    self._readdir(sock, parent_path)
     return True
 
-  def mv(self, old, new):
+  @inject_socket
+  def mv(self, sock, old, new):
     if not self._fs.has(old):
       raise RuntimeError('{0} not exist'.format(old))
     id = self._fs.getid(old)
@@ -120,12 +138,12 @@ class Client():
       raise RuntimeError('target {0} not exist'.format(head))
     parent_id = self._fs.getid(head)
     if meta['type'] == 'file':
-      return self._mvfile(id, parent_id, tail)
+      return self._mvfile(id, sock, parent_id, tail)
     else:
-      return self._mvdir(id, parent_id, tail)
+      return self._mvdir(id, sock, parent_id, tail)
 
-  def _mvfile(self, id, parent_id, name):
-    _, body = self.request('file#mvfile', header = {
+  def _mvfile(self, sock, id, parent_id, name):
+    _, body = self.request(sock, 'file#mvfile', header = {
       'id': id,
       'pdid': parent_id,
       'name': name
@@ -134,8 +152,8 @@ class Client():
       raise ServerError('mvfile fail')
     return True
 
-  def _mvdir(self, id, parent_id, name):
-    _, body = self.request('dir#mvdir', header = {
+  def _mvdir(self, sock, id, parent_id, name):
+    _, body = self.request(sock, 'dir#mvdir', header = {
       'id': id,
       'pdid': parent_id,
       'name': name
@@ -144,59 +162,63 @@ class Client():
       raise ServerError('mvdir fail')
     return True
 
-  def readdir(self, path):
+  @inject_socket
+  def readdir(self, sock, path):
     cache_key = '{0}:cache'.format(path)
     if self._cache and self._rlite.exists(cache_key):
-      dirents = self._fs.readdir(path)
+      dirents = self._fs.readdir(sock, path)
       logger.info('cached dirents: %s', dirents)
       return dirents
-    return self._readdir(path)
+    return self._readdir(sock, path)
 
-  def _readdir(self, path):
+  def _readdir(self, sock, path):
     logger.info('_readdir path: %s', path)
     cache_key = '{0}:cache'.format(path)
     self._rlite.expire(cache_key, '0')
     id = self._fs.getid(path)
     logger.info('_readdir id: %s', id)
-    data = self._readdir_with_id(id)
+    data = self._readdir_with_id(sock, id)
     self._fs.adddir(path, data)
     return data.keys()
 
-  def _readdir_with_id(self, id = None):
-    _, body = self.request('dir#list', header = { 'id': id })
+  def _readdir_with_id(self, sock, id = None):
+    _, body = self.request(sock, 'dir#list', header = { 'id': id })
     data = json.loads(body.decode('utf-8'))
     return data
 
-  def mkdir(self, path, name):
+  @inject_socket
+  def mkdir(self, sock, path, name):
     parent_id = self._fs.getid(path)
-    _, body = self.request('dir#add', header = { 'id': parent_id, 'name': name })
+    _, body = self.request(sock, 'dir#add', header = { 'id': parent_id, 'name': name })
     if body != b'OK':
       raise ServerError('Mkdir fail')
     return self._readdir(path)
 
+  @inject_socket
   def rmdir(self, path):
     id = self._fs.getid(path)
-    _, body = self.request('dir#rm', header = { 'id': id })
+    _, body = self.request(sock, 'dir#rm', header = { 'id': id })
     if body != b'OK':
       raise ServerError('Rmdir fail')
     self._readdir(path)
     return True
 
-  def request(self, request, body = b'', header = {}):
+  def request(self, sock, request, body = b'', header = {}):
     controller, action = request.split('#')
     logger.info('Request: action: %s, header: %s', action, header)
     _header = { 'controller': controller, 'action': action }
     _header.update(header)
-    self._send(Packet(_header, body))
-    pkt = self._read_response()
+    self._send(sock, Packet(_header, body))
+    pkt = self._read_response(sock)
     if not pkt:
       raise DisconnectError('connection lost')
     return (pkt.headers, pkt.body)
 
-  def send(self, packet):
+  @inject_socket
+  def send(self, sock, packet):
     if type(packet) is not Packet:
       raise TypeError('Must be Packet')
-    self._send(packet)
+    self._send(sock, packet)
 
   def _init(self):
     self.login()
@@ -204,39 +226,37 @@ class Client():
     self._rlite.flushall()
     self._init_root()
 
-  def _init_root(self):
-    data = self._readdir_with_id(1)
+  @inject_socket
+  def _init_root(self, sock):
+    data = self._readdir_with_id(sock, 1)
     assert data['.']['id'] == 1
     self._fs.adddir('/', data)
 
-  def _send(self, packet):
+  def _send(self, sock, packet):
     data = packet.to_bytes()
     logger.info('_send: Send: %s', packet.headers)
-    self._socket.sendall(data)
+    sock.sendall(data)
 
   def reconnect(self):
-    if self._socket:
-      self.close()
-    self._connect()
     self._init()
 
   def _connect(self):
-    logger.info('Host: %s, Port: %s', self._host, self._port)
-    self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.info('Create connection')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-      self._socket.connect((self._host, self._port))
+      sock.connect((self._host, self._port))
     except ConnectionError:
       logger.error('Connection fail')
       sys.exit('Connection fail')
+    return sock
 
   def close(self):
-    self._socket.close()
-    self._socket = None
+    pass
 
-  def _read_response(self):
+  def _read_response(self, sock):
     logger.info('Read response')
     try:
-      buf = self._socket.recv(4096)
+      buf = sock.recv(4096)
     except socket.timeout:
       raise TimeoutError()
     if len(buf) == 0:
@@ -247,7 +267,7 @@ class Client():
         break
       if not pkt.check():
         try:
-          buf = self._socket.recv(4096)
+          buf = sock.recv(4096)
         except socket.timeout:
           raise TimeoutError()
         if len(buf) == 0:
